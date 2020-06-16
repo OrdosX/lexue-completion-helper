389d76d395a7893fa4cc1f8dd9c0b2652082c7e1
OrdosX <i@ordosx.tech>
2020-06-16 18:27:55 +0800

    1.1.0 - 优化登录

--- asyncGetCookie.js
+++ asyncGetCookie.js
@@ -41,23 +41,43 @@ var fs = require("fs");
 var got_1 = require("got");
 require("dotenv").config();
 exports["default"] = (function () { return __awaiter(void 0, void 0, void 0, function () {
-    var loginPage, $, ticket, tempCookie, finalCookie;
+    var cookie, home, loginPage, $, ticket, tempCookie, finalCookie;
     return __generator(this, function (_a) {
         switch (_a.label) {
             case 0:
-                // 如果缓存了cookie，直接返回
-                if (fs.existsSync("./.cookie") && fs.readFileSync("./.cookie").length != 0) {
-                    return [2 /*return*/, fs.readFileSync("./.cookie")];
+                if (!(fs.existsSync("./.cookie") && fs.readFileSync("./.cookie").length != 0)) return [3 /*break*/, 2];
+                cookie = fs.readFileSync("./.cookie");
+                return [4 /*yield*/, got_1["default"].get("http://lexue.bit.edu.cn/my/", {
+                        followRedirect: false,
+                        headers: {
+                            'Host': 'lexue.bit.edu.cn',
+                            'Connection': 'keep-alive',
+                            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36',
+                            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+                            'Accept-Encoding': 'gzip, deflate',
+                            'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,zh-TW;q=0.7',
+                            'cookie': cookie.toString()
+                        }
+                    })];
+            case 1:
+                home = _a.sent();
+                if (home.statusCode != 200) {
+                    console.log('cookie缓存过期，尝试登录 / Cookie out of date, attempt to login');
                 }
-                //如果没有设置用户名和密码，报错退出
-                if (process.env.BIT_ID == undefined || process.env.PASSWORD == undefined) {
-                    console.log('未设置用户名和密码 / Failed to read student ID and password');
-                    process.exit(-1);
+                else {
+                    return [2 /*return*/, cookie];
+                }
+                _a.label = 2;
+            case 2:
+                //如果没有设置用户名或密码，报错退出
+                if (process.env.BIT_ID === "" || process.env.PASSWORD === "") {
+                    console.log('未设置用户名或密码 / Failed to read student ID or password');
+                    process.exit(0);
                 }
                 return [4 /*yield*/, got_1["default"].get("https://login.bit.edu.cn/cas/login?service=http%3A%2F%2Flexue.bit.edu.cn%2Flogin%2Findex.php", {
                         followRedirect: false
                     })];
-            case 1:
+            case 3:
                 loginPage = _a.sent();
                 $ = cheer.load(loginPage.body);
                 return [4 /*yield*/, got_1["default"].post("https://login.bit.edu.cn/cas/login?service=http%3A%2F%2Flexue.bit.edu.cn%2Flogin%2Findex.php", {
@@ -82,12 +102,17 @@ exports["default"] = (function () { return __awaiter(void 0, void 0, void 0, fun
                             'cookie': loginPage.headers["set-cookie"][0].slice(0, 48)
                         }
                     })];
-            case 2:
+            case 4:
                 ticket = _a.sent();
+                if (ticket.statusCode == 200) {
+                    // 登陆信息错误 => 200，信息正确 => 302
+                    console.log('用户名或密码错误 / Invalid student ID or password');
+                    process.exit(0);
+                }
                 return [4 /*yield*/, got_1["default"].get(ticket.headers.location, {
                         followRedirect: false
                     })];
-            case 3:
+            case 5:
                 tempCookie = _a.sent();
                 return [4 /*yield*/, got_1["default"].get(tempCookie.headers.location, {
                         followRedirect: false,
@@ -101,9 +126,10 @@ exports["default"] = (function () { return __awaiter(void 0, void 0, void 0, fun
                             'Cookie': tempCookie.headers["set-cookie"][0].split(";")[0]
                         }
                     })];
-            case 4:
+            case 6:
                 finalCookie = _a.sent();
                 fs.writeFileSync("./.cookie", finalCookie.headers["set-cookie"][0].split(";")[0]);
+                console.log('登陆成功 / Login succeed');
                 return [2 /*return*/, finalCookie.headers["set-cookie"][0].split(";")[0]];
         }
     });
--- asyncGetCookie.ts
+++ asyncGetCookie.ts
@@ -4,14 +4,32 @@ import got from "got";
 require("dotenv").config();
 
 export default async () => {
-    // 如果缓存了cookie，直接返回
+    // 如果缓存了cookie，验证成功后直接返回
     if(fs.existsSync("./.cookie") && fs.readFileSync("./.cookie").length != 0) {
-        return fs.readFileSync("./.cookie");
+        let cookie = fs.readFileSync("./.cookie");
+        // 尝试获取主页，如果被重定向则是cookie过期，重新登陆
+        let home = await got.get("http://lexue.bit.edu.cn/my/", {
+            followRedirect: false,
+            headers: {
+                'Host': 'lexue.bit.edu.cn',
+                'Connection': 'keep-alive',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36',
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+                'Accept-Encoding': 'gzip, deflate',
+                'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,zh-TW;q=0.7',
+                'cookie': cookie.toString()
+            }
+        });
+        if(home.statusCode != 200) {
+            console.log('cookie缓存过期，尝试登录 / Cookie out of date, attempt to login');
+        } else {
+            return cookie;
+        }
     }
-    //如果没有设置用户名和密码，报错退出
-    if(process.env.BIT_ID == undefined || process.env.PASSWORD == undefined) {
-        console.log('未设置用户名和密码 / Failed to read student ID and password');
-        process.exit(-1);
+    //如果没有设置用户名或密码，报错退出
+    if(process.env.BIT_ID  === "" || process.env.PASSWORD  === "") {
+        console.log('未设置用户名或密码 / Failed to read student ID or password');
+        process.exit(0);
     }
     // 否则执行登录流程
     let loginPage = await got.get("https://login.bit.edu.cn/cas/login?service=http%3A%2F%2Flexue.bit.edu.cn%2Flogin%2Findex.php", {
@@ -40,6 +58,11 @@ export default async () => {
             'cookie': loginPage.headers["set-cookie"][0].slice(0, 48)
         }
     })
+    if(ticket.statusCode == 200) {
+        // 登陆信息错误 => 200，信息正确 => 302
+        console.log('用户名或密码错误 / Invalid student ID or password');
+        process.exit(0);
+    }
     let tempCookie = await got.get(ticket.headers.location, {
         followRedirect: false
     });
@@ -56,5 +79,6 @@ export default async () => {
         }
     });
     fs.writeFileSync("./.cookie", finalCookie.headers["set-cookie"][0].split(";")[0]);
+    console.log('登陆成功 / Login succeed');
     return finalCookie.headers["set-cookie"][0].split(";")[0];
 }
